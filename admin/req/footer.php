dc38556e2277d0afc147df53e8c4a2a9be2d9b5f
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-09-17 22:49:12 +0800

    Add files via upload

---
+++ admin/req/footer.php
@@ -0,0 +1,40 @@
+
+
+                </div>
+                <!-- /.container-fluid -->
+            </div>
+            <!-- /#page-wrapper -->
+
+
+</div>
+        <!-- /#wrapper -->
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/629cb8807b967b117992fbb7/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<script src="https://www.w3counter.com/tracker.js?id=145497"></script>
+<!--End of Tawk.to Script--><!-- BODY EOF   -->
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="js/metisMenu.min.js"></script>
+
+        <!-- Morris Charts JavaScript -->
+        <script src="js/raphael.min.js"></script>
+        <!-- <script src="js/morris.min.js"></script> -->
+        <!-- <script src="js/morris-data.js"></script> -->
+
+        <!-- Custom Theme JavaScript -->
+        <script src="js/startmin.js"></script>
+    </body>
+</html>
