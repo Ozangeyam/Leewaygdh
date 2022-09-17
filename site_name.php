0815b9a9935291d5a7dc5713cc81f5331e48393d
Snoodie <45816295+Ozangeyam@users.noreply.github.com>
2022-09-17 23:09:24 +0800

    Add files via upload

---
+++ site_name.php
@@ -0,0 +1,10 @@
+<?php 
+
+$site_title = "LeeWaygdh";
+$site_url = "https://LeeWaygdh.com";
+$support_email = "support@LeeWaygdh.com";
+$company_address = "300 Vesey Street, New York, NY 10282 United States";
+$site_logo = $site_url . "/img/up_logo.png";
+$site_logo2 = $site_url . "/img/up_logo2.png";
+$cert = $site_url . "/en/img/cert.png";
+$favicon = $site_url . "/img/favicon.png";
